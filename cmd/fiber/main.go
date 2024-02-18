3c9533243048cd428bf3206f14d21a86273212b4
Arturs Adejanovs <arturs.adejanovs@gmail.com>
2024-02-18 15:00:18 +0200

    Graceful shutdown

--- cmd/fiber/main.go
+++ cmd/fiber/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"syscall"
 
 	"github.com/aadejanovs/wallet/internal/infrastructure"
+	"github.com/vrecan/death"
 )
 
 func main() {
 	server := infrastructure.Setup()
-	log.Fatal(server.Listen("0.0.0.0:80"))
+
+	go func() {
+		err := server.Listen("0.0.0.0:80")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	d := death.NewDeath(syscall.SIGINT, syscall.SIGTERM)
+
+	d.WaitForDeathWithFunc(func() {
+		fmt.Println("server_shutting_down")
+
+		err := server.Shutdown()
+		if err != nil {
+			fmt.Printf("server_shutdown_error: %s\n", err)
+		}
+	})
+
+	d.WaitForDeath()
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -43,6 +43,7 @@ require (
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/vrecan/death v3.0.1+incompatible // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
